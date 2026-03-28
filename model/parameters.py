a5c1f20d1f651d9b52e2a366f25f8587286dbdba
hruliu26 <han.r.liu@mail.mcgill.ca>
2026-03-27 21:38:34 -0400

    changed parameter values

--- model/parameters.py
+++ model/parameters.py
@@ -1,19 +1,19 @@
 # parameter ranges in units to be displayed
 param_ranges = {
-    'template': (0, 10, 0.01),    # circular DNA template in ng/µL
-    'polymerase': (0, 1, 0.01),   # phi29 DNA polymerase in U/µL
-    'primers': (0, 5, 0.1),       # µM
-    'dNTPs': (0, 20, 0.1),        # mM
-    'Mg2': (0, 10, 0.1),          # mM
+    'template': (0, 20, 1),    # circular DNA template in ng/µL
+    'polymerase': (0, 2, 0.1),   # phi29 DNA polymerase in U/µL
+    'primers': (0, 100, 1),       # µM
+    'dNTPs': (0, 20, 1),        # mM
+    'Mg2': (0, 20, 1),          # mM
     'time': (0, 4*60, 60),       # range of 0 to 4 hours
 }
 
 # default parameters 
 default_params = {
-    'template': 1,
+    'template': 10,
     'polymerase': 0.5,
-    'primers': 0.5,
-    'dNTPs': 10,
-    'Mg2': 5,
-    'time': 120,  # in minutes
+    'primers': 25,
+    'dNTPs': 5,
+    'Mg2': 15,
+    'time': 180,  # in minutes
 }
